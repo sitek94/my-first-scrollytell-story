088c0222f1791a056382c9a9d1427ce2abf06377
Maciek <msitkowski94@gmail.com>
2022-10-21 07:53:16 +0200

    add Scrolly and show current step

---
+++ src/lib/Scrolly.svelte
@@ -0,0 +1,82 @@
+<script>
+  /**
+   * This component manages which item is most in view for scroll triggering
+   * example:
+   * <Scrolly
+   * 	bind:value={scrollIndex}
+   * >
+   * **items here**
+   * </Scrolly>
+   *
+   * optional params with defaults
+   * <Scrolly root={null} top={0} bottom={0} increments={100}>
+   *
+   * https://svelte.dev/repl/3d3736e634c9404ea8ec2ef7b87e2053?version=3.42.4
+   */
+  import { onMount } from "svelte";
+  export let root = null;
+  export let top = 0;
+  export let bottom = 0;
+  export let increments = 100;
+
+  export let stepIndex = undefined;
+  export let stepsRatios = [];
+
+  const threshold = [];
+  let nodes = [];
+  let intersectionObservers = [];
+  let container;
+
+  $: top, bottom, update();
+
+  const update = () => {
+    if (!nodes.length) return;
+    nodes.forEach(createObserver);
+  };
+
+  const mostInView = () => {
+    let maxRatio = 0;
+    let maxIndex = 0;
+
+    for (let i = 0; i < stepsRatios.length; i++) {
+      if (stepsRatios[i] > maxRatio) {
+        maxRatio = stepsRatios[i];
+        maxIndex = i;
+      }
+    }
+
+    return maxRatio > 0 ? maxIndex : undefined;
+  };
+
+  const createObserver = (node, index) => {
+    const handleIntersect = (entries) => {
+      stepsRatios[index] = entries[0].intersectionRatio;
+      stepIndex = mostInView();
+    };
+
+    const marginTop = top ? top * -1 : 0;
+    const marginBottom = bottom ? bottom * -1 : 0;
+    const rootMargin = `${marginTop}px 0px ${marginBottom}px 0px`;
+    const options = { root, rootMargin, threshold };
+
+    if (intersectionObservers[index]) {
+      intersectionObservers[index].disconnect();
+    }
+
+    const observer = new IntersectionObserver(handleIntersect, options);
+    observer.observe(node);
+    intersectionObservers[index] = observer;
+  };
+
+  onMount(() => {
+    for (let i = 0; i < increments + 1; i++) {
+      threshold.push(i / increments);
+    }
+    nodes = container.querySelectorAll(":scope > *");
+    update();
+  });
+</script>
+
+<div class="scrolly" bind:this={container}>
+  <slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,59 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Scrolly from '$lib/Scrolly.svelte';
+
+	let stepIndex = 0;
+	let stepsRatios = [];
+
+	$: {
+		console.log(stepIndex);
+		console.log(stepsRatios);
+	}
+
+	console.log(stepIndex);
+	console.log(stepsRatios);
+</script>
+
+<header>
+	<h1>Scrollytelling</h1>
+</header>
+
+<div class="stats">
+	<p>Current step: {stepIndex}</p>
+</div>
+
+<Scrolly bind:stepIndex bind:stepsRatios>
+	<section>1</section>
+	<section>2</section>
+	<section>3</section>
+	<section>4</section>
+</Scrolly>
+
+<style>
+	:root {
+		color-scheme: dark;
+	}
+
+	:global(html) {
+		font-family: sans-serif;
+	}
+
+	.stats {
+		position: fixed;
+		top: 0;
+		left: 1rem;
+	}
+
+	header {
+		text-align: center;
+	}
+
+	section {
+		height: 60vh;
+		width: 500px;
+		border: 3px dashed #ccc;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		margin: 0 auto 10vh;
+	}
+</style>
